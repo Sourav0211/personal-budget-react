59e05fdba024deb3b9d3a79cc4f90e15874ecac1
sourav11 <143427277+Sourav0211@users.noreply.github.com>
2023-09-29 12:15:14 -0400

    Brought PBudget code in

---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <footer className="bottom">
+    <div className="center">
+        All rights reserved &copy; Sourav Shetye
+    </div>
+</footer>
+  );
+}
+
+export default Footer;
